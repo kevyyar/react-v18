8984f68d65ddab8359bc994d5df9a8fadcca41eb
Kevin Barreto <barretokevin@hotmail.com>
2023-03-27 13:29:15 -0600

    change code to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom'
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import { createRoot } from 'react-dom/client'
+import Pet from './Pet'
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      animal: "Dog",
-      name: "Luna",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      animal: "Bird",
-      name: "Pepper",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { animal: "Cat", name: "Doink", breed: "Mixed" }),
-  ]);
-};
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet animal="Dog" name="Luna" breed="Havanese" />
+    </div>
+  )
+}
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
