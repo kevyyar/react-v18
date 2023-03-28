893c84e8c68a493b8e346fb95901b41e57010ebb
Kevin Barreto <barretokevin@hotmail.com>
2023-03-28 11:50:45 -0600

    add useEffect to fetch data and render on page

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Pet from './Pet'
 
 const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile']
 
@@ -6,11 +7,26 @@ const SearchParams = () => {
   const [location, setLocation] = useState('')
   const [animal, setAnimal] = useState('')
   const [breed, setBreed] = useState('')
+  const [pets, setPets] = useState([])
   const breeds = []
+
+  useEffect(() => {
+    requestPets()
+  }, [])
+
+  async function requestPets() {
+    const res = await fetch(`http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`)
+    const json = await res.json()
+
+    setPets(json.pets)
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        requestPets()
+      }}>
         <label htmlFor="location">
           Location
           <input
@@ -52,6 +68,9 @@ const SearchParams = () => {
         </select>
         <button type="submit">Submit</button>
       </form>
+      {pets.map(pet => (
+        <Pet key={pet.id} name={pet.name} animal={pet.name} breed={pet.breed} />
+      ))}
     </div>
   )
 }
