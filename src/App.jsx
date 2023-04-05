c2f9d65ab2b6c19fcdc4f36494858e7558f85d73
Kevin Barreto <barretokevin@hotmail.com>
2023-04-05 14:36:55 -0600

    add code splitting to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,46 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AdoptedPetContext from './AdoptedPetContext';
-import SearchParams from './SearchParams';
-import Details from './Details';
+
+const Details = lazy(() => import('./Details'))
+const SearchParams = lazy(() => import('./SearchParams'))
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: Infinity,
-      cacheTime: Infinity
+    defaultOptions: {
+        queries: {
+            staleTime: Infinity,
+            cacheTime: Infinity
+        }
     }
-  }
 })
 
 const App = () => {
-  const adoptedPet = useState(null)
-  return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">
-              Adopt Me!
-            </Link>
-          </header>
-          <Routes>
-            <Route path='/details/:id' element={<Details />} />
-            <Route path='/' element={<SearchParams />} />
-          </Routes>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-    </BrowserRouter >
-  )
+    const adoptedPet = useState(null)
+    return (
+        <BrowserRouter>
+            <AdoptedPetContext.Provider value={adoptedPet}>
+                <QueryClientProvider client={queryClient}>
+                    <Suspense fallback={
+                        <div className='loading-pane'>
+                            <h2 className='loader'>loading...!</h2>
+                        </div>
+                    }>
+                        <header>
+                            <Link to="/">
+                                Adopt Me!
+                            </Link>
+                        </header>
+                        <Routes>
+                            <Route path='/details/:id' element={<Details />} />
+                            <Route path='/' element={<SearchParams />} />
+                        </Routes>
+                    </Suspense>
+                </QueryClientProvider>
+            </AdoptedPetContext.Provider>
+        </BrowserRouter >
+    )
 }
 const container = document.getElementById("root");
 const root = createRoot(container);
